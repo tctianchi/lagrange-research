18166f8f96d636dc7cd2e41d16f524556750ae12
tctianchi <7827456+tctianchi@users.noreply.github.com>
2023-08-04 23:28:49 +0800

    keep xlsx the same as site

--- src/data/excelParser.py
+++ src/data/excelParser.py
@@ -12,29 +12,30 @@ def load(open_file):
 	r2 = {}
 	r3 = {}
 	keys = df.columns.tolist()
-	companys = keys[3:7]
-	strategies = keys[7:11]
-	tactics = keys[11:]
+	companys = keys[4:9]
+	strategies = keys[9:13]
+	tactics = keys[13:15]
 	data = []
 	for row in df.itertuples():
 		item = {
 			'id': row.Index,
 			'class': row[1],
+			"rate": int(row[4]) if int(row[4]) == row[4] else row[4],
 			'prototype_name': row[2],
 			'subtype': row[3] if pd.notnull(row[3]) else None
 		}
-		for x in range(4,8):
+		for x in range(5,10):
 			if pd.notnull(row[x]):
-				item['company'] = companys[x-4]
+				item['company'] = companys[x-5]
 				break
 
 		item['strategy'] = []
-		for x in range(8,12):
+		for x in range(10,14):
 			if pd.notnull(row[x]):
-				item['strategy'].append(strategies[x-8]) 
+				item['strategy'].append(strategies[x-10]) 
 		
 		item['tactic'] = []
-		for x in range(12,14):
+		for x in range(14,16):
 			if pd.notnull(row[x]):
 				item['tactic'].append(tactics[x-14])
 		data.append(item)
